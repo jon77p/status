37f2cd0a956d5228bac4a137d379dc77850948bc
Jon Prentice <jon77p@gmail.com>
2019-04-30 16:49:01 -0700

    Moved logging to separate folder for project

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 CURR="$(pwd)"
+USER="$(whoami)"
 
 sudo ln -s $CURR/etc/systemd/system/status.service /etc/systemd/system/status.service
 
 sudo ln -s $CURR/etc/nginx/sites-available/status /etc/nginx/sites-available/status
 sudo ln -s /etc/nginx/sites-available/status /etc/nginx/sites-enabled/status
+
+sudo mkdir -p /var/log/status
+sudo chown $USER:$USER /var/log/status
 
 sudo systemctl daemon-reload
 sudo systemctl start status.service
